14a89ad495d5d66ed474db106aa72a287dfa3a65
[esekyi] <[sskert10@gmail.com]>
2024-08-24 20:45:29 +0000

    [Implement🦾] user services implents

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -1,8 +1,27 @@
 from app.models.user import User
+from app import db
 
 def get_all_users():
 	return User.query.all()
 
-def create_user(username, email):
-	user = User(username=username, email=email)
-	return user
+
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
+
+
+def create_user(username, email, password_hash):
+	new_user = User(username=username, email=email, password_hash=password_hash)
+	return new_user
+
+
+def update_user(user, email=None, password_hash=None):
+    if email:
+        user.email = email
+    if password_hash:
+        user.password_hash = password_hash
+    db.session.commit()
+
+
+def delete_user(user):
+    db.session.delete(user)
+    db.session.commit()
