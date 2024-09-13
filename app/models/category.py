24fda1cca32b5aee27db238b9528ece864fa3a72
[esekyi] <[sskert10@gmail.com]>
2024-09-13 01:21:43 +0000

    [Fixes🛠️] removed deprecated utcnow() from models fields.

--- app/models/category.py
+++ app/models/category.py
@@ -1,5 +1,5 @@
 from app import db
-from datetime import datetime
+from datetime import datetime, timezone
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -8,9 +8,10 @@ class Category(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(
+        db.DateTime, default=datetime.now(timezone.utc))
     updated_at = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+        db.DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     def __repr__(self):
         return f'<Category {self.name}>'
