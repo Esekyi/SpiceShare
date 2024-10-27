6997ae9cddb7af1900fa8b5602371159abb93769
[esekyi] <[sskert10@gmail.com]>
2024-10-27 13:07:15 +0000

    [Updates📢] implemented reCAPTCHA service on auth and subscribe routes to prevent bot attacks.

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash
 from app.models.user import User
-from app import db
+from app.services.recaptcha_service import verify_recaptcha
 
 """Route for basic authentication"""
 
@@ -22,6 +22,8 @@ def is_safe_url(target):
 @auth_bp.route('/login', methods=['GET', 'POST'], strict_slashes=False)
 def login():
 	"""Login authentication handler"""
+
+
 	# Check if the user is already authenticated
 	if current_user.is_authenticated:
 		return redirect(url_for('user_routes.user_profile', user_id=current_user.id))
@@ -31,6 +33,11 @@ def login():
 		password = request.form["password"]
 		next_page = request.form.get("next")
 		remember_me = request.form.get('remember_me')
+
+		recaptcha_response = request.form.get('g-recaptcha-response')
+		if not recaptcha_response or not verify_recaptcha(recaptcha_response):
+			flash("CAPTCHA verification failed. Please try again.", "error")
+			return redirect(url_for('auth.login'))
 
 		# Convert checkbox to boolean
 		remember = True if remember_me == "on" else False
